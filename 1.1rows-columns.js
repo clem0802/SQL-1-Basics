c1254c7cab78e8866c6d35e704f1594d6b7d7481
clem0802 <88548513+clem0802@users.noreply.github.com>
2022-02-12 23:09:59 +0100

    Create 1.1rows-columns.js

---
+++ 1.1rows-columns.js
@@ -0,0 +1,24 @@
+SQL (2022.02.12) - Mimo app
+// many apps we interact with store data like a username, password, or friends list
+// this kind of data lives in a "database"
+// most databases are collections of "tables" storing different pieces of data
+
+
+// SQL is a language for modifying, inserting, and accessing data from tables in a database
+// tables are like spreadsheets storing information about a specific kind of data, 
+// like this table sotring users' IDs, names, and emails
+// each row of a table represents a new item
+// below there are "3 user items", because it has three ROWS
+// if rows stand for items, COLUMNS are "properties of each user item, like a (name) or (email)"
+
+__________________________________
+| Users                           |
+-----------------------------------
+| userID | name | email           |
+-----------------------------------
+| 1      | Sam  | sam17@mail.com  |
+-----------------------------------
+| 2      | Remy | rem@mail.com    |
+-----------------------------------
+| 3      | Luis | luis_99@mail.com|
+-----------------------------------
