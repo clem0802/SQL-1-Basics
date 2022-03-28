1ea1f2e8c2dc66386fc9bea5c6ba08b162f04a5c
clem0802 <88548513+clem0802@users.noreply.github.com>
2022-03-28 15:32:47 +0200

    Update 2.1-ORDER-BY.js

--- 2.1-ORDER-BY.js
+++ 2.1-ORDER-BY.js
@@ -14,7 +14,43 @@ _______________________
 | Theophin   | 28     |
 -----------------------
 // understanding data is easier when the items are ordered, 
-// like "patients" ordered by their "name"
+// like "patients" ORDER BY their "name", alphabetically
   SELECT *
   FROM patients
   ORDER BY name
+// name      age
+// Jones     60
+// Smith     19
+// Theophin  28
+// Wilson    45
+  
+ 
+/*------------------------------------------------------------------*/
+__________________________
+| countries              |
+--------------------------
+| name       | gdp       |
+--------------------------
+| Greece     | 187.46    |
+--------------------------
+| Sweden     | 474.15    |
+--------------------------
+| Iceland    | 21.6      |
+--------------------------
+| Germany    | 3449.05   |
+--------------------------
+// rearrange the items in the "countries" table with "ORDER BY"
+SELECT *
+FROM countries
+ORDER BY name;
+// name      gdp
+// Germany   3449.05
+// Greece    187.46
+// Iceland   21.6
+// Sweden    474.15
+
+
+
+
+
+
