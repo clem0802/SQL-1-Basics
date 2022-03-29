26f91bb57ee1569d3470108c83c1dbd569316088
clem0802 <88548513+clem0802@users.noreply.github.com>
2022-03-29 13:37:31 +0200

    Create 6.1-IN.js

---
+++ 6.1-IN.js
@@ -0,0 +1,55 @@
+SQL (2022.03.29) - Mimo app (SQL - IN)
+
+______________________________
+| customers                  |
+------------------------------
+| name          | countries  |
+------------------------------
+| Maria Sousa   | Brazil     |
+------------------------------
+| Paul Vagner   | Germany    |
+------------------------------
+| Sara Sato     | Japan      |
+------------------------------
+| Louis Martin  | France     |
+------------------------------
+// use "IN" keyword to specify options when intende for (Germany OR France)
+// after "IN" keyword, we code the options between parentheses => ()
+// the two conditions are separately inside simple quotes => 'France','Germany'
+SELECT *
+FROM customers
+WHERE country IN ('France', 'Germany');
+// name            country  
+// Paul Wagner     Germany
+// Louis Martin    France
+
+SELECT *
+FROM customers
+WHERE country IN ('France', 'Germany', 'Japan');
+// name            country  
+// Paul Wagner     Germany
+// Sara Sato       Japan
+// Louis Martin    France
+
+/*------------------------------------------------------------------*/
+__________________________
+| customers               |
+--------------------------
+| name          | orders  |
+--------------------------
+| Maria Sousa   | 8       |
+--------------------------
+| Paul Vagner   | 0       |
+--------------------------
+| Sara Sato     | 5       |
+--------------------------
+| Louis Martin  | 1       |
+--------------------------
+
+SELECT *
+FROM customers
+WHERE orders IN (0, 8);
+// name            country  
+// Maria Sousa     8
+// Paul Wagner     0
+
